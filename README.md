9c6b72d810bfdf5fc206b2ee5252cbd62ac1e7dc
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-03-30 11:58:28 -0500

    Something new learned and new exercise with lists

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 - Easy to learn.
 
 - Elegant (defined structure).
+
+## Learned
+
+- The advantage of tuples over lists is that going through a for loop is much faster with tuples.
+
+`lista = [1, 2, 3, 4, 5]`
+` `
+`tupla = (1, 2, 3, 4, 5)`
+
---
+++ some_problems/pass_generator.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import random
+
+def generate_passw():
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    characters = MAYUS + MINUS + NUMS + CHARS
+    passw = []
+
+    for i in range(6):
+        char_rand = random.choice(characters)
+        passw.append(char_rand)
+    passw = ''.join(passw)
+    return passw
+
+def run():
+    password = generate_passw()
+    print("Tu nueva contraseña es: " + password)
+
+if __name__ == '__main__':
+    run()
