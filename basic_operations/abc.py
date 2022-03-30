1d83a4ba9de9ad3170f933e23034baaa6f0a3324
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-03-30 11:35:55 -0500

    Add basic operations

---
+++ basic_operations/abc.py
@@ -0,0 +1,26 @@
+my_var = "Tipos de datos en python\n"
+my_var += "bool, int, float, str\n"
+
+print (my_var)
+
+print ("Operadores aritméticos básicos")
+print ("suma", 5 + 5)
+print ("resta", 5 - 5)
+print ("multiplicación", 2*5)
+print ("división", 21/5)
+print ("división parte entera", 21//5)
+print ("módulo", 21%5)
+print ("potencia", 2**3, "\n")
+
+print ("Operadores comparación")
+print ("== es igual")
+print ("!= es distinto")
+print ("> es mayor")
+print ("< es menor")
+print (">= es mayor o igual")
+print ("<= es menor o igual \n")
+
+print ("Operadores booleanos")
+print ("and es intersección")
+print ("or es unión")
+print ("not es negación")
