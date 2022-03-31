bfb5d929960da8503f44edd6bc1cd484ceb320e1
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-03-31 11:33:56 -0500

    Palindrome exercise with lambda function

---
+++ withenv/python_m_course/palindrome.py
@@ -0,0 +1,9 @@
+palindrome = lambda param: param == param[::-1]
+print(palindrome('ana'))
+print(palindrome('tucson'))
+print()
+
+def palindrome_b(param):
+	return param == param[::-1]
+print(palindrome_b('ana'))
+print(palindrome_b('tucson'))
