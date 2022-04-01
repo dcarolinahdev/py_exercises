6e1261ee1e15a934f979773fed17ab5d584bfb11
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-04-01 09:43:44 -0500

    Catch type errors in palindrome exercise

--- withenv/python_m_course/palindrome.py
+++ withenv/python_m_course/palindrome.py
@@ -1,9 +1,14 @@
-palindrome = lambda param: param == param[::-1]
+# Lambda form
+# palindrome = lambda param: param == param[::-1]
+
+def palindrome(param):
+	return param == param[::-1]
+
 print(palindrome('ana'))
 print(palindrome('tucson'))
-print()
 
-def palindrome_b(param):
-	return param == param[::-1]
-print(palindrome_b('ana'))
-print(palindrome_b('tucson'))
+# First try-except block code
+try:
+	print(palindrome(1))
+except TypeError:
+	print("You only can type strings")
