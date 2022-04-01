92292c6847f3ae6eda9c1201a77fcea4bd96dc4f
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-04-01 10:26:06 -0500

    Assert statement for palindrome exercise

--- withenv/python_m_course/palindrome.py
+++ withenv/python_m_course/palindrome.py
@@ -2,20 +2,27 @@
 # palindrome = lambda param: param == param[::-1]
 
 # Second try-except block code
-def palindrome(param):
-	try:
-		if len(param) == 0:
-			raise ValueError("You can't type an empty string")
-		return param == param[::-1]
-	except ValueError as ve:
-		print(ve)
-		return False
+# def palindrome(param):
+# 	try:
+# 		if len(param) == 0:
+# 			raise ValueError("You can't type an empty string")
+# 		return param == param[::-1]
+# 	except ValueError as ve:
+# 		print(ve)
+# 		return False
 
-print(palindrome('ana'))
-print(palindrome('tucson'))
+# print(palindrome('ana'))
+# print(palindrome('tucson'))
 
 # First try-except block code
 # try:
 # 	print(palindrome(''))
 # except TypeError:
 # 	print("You only can type strings")
+
+# With assert statements
+def palindrome(param):
+	assert len(param) > 0, "You can't type an empty string"
+	return param == param[::-1]
+
+print(palindrome(''))
